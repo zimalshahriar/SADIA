58f8397a91a1dc643c2d1c59dc89af6f1225e5c9
zimalshahriar <sz.zim2050@gmail.com>
2025-08-09 16:57:10 +0600

    fix: update text color in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 text-4xl font-bold text-pink-700">
+    <div className="flex items-center justify-center h-screen bg-gray-100 text-4xl font-bold text-yellow-700">
       SADIA is Live 🚀
     </div>
   );
